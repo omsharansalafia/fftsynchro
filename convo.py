40907d24565435a70edcecac4e148d28861f8601
Om Sharan Salafia <omsharan.salafia@gmail.com>
2024-03-06 10:26:23 +0100

    Updates

--- convo.py
+++ convo.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.signal import convolve
 from .synchro import normalization_Pnu
 from .synchro import x0,F0
+from .synchro import P_nu
 from .constants import *
 
 def gxy(x):
@@ -10,7 +11,7 @@ def gxy(x):
     """
     return np.interp(np.exp(x),x0,F0)
 
-def j_nu(nu,dn_dg,B,pitch,m=me,q=e,gmin=1.,gmax=1e8,res=0.1):
+def j_nu(nu,dn_dg,B,pitch,m=me,q=e,gmin=1.,gmax=1e8,res=0.1,mode='fft'):
     """
     Specific synchrotron emissivity (power per unit frequency, per unit 
     volume, per unit steradian) of a population of particles whose density
@@ -32,38 +33,52 @@ def j_nu(nu,dn_dg,B,pitch,m=me,q=e,gmin=1.,gmax=1e8,res=0.1):
     - gmax: maximum Lorentz factor to be considered
     - res: resolution, understood as the amplitude of logarithmic 
            frequency steps in the grid
+    - mode: either 'fft' or 'direct'
     """
     
-    # define convenience function for convolution
-    def fy(y):
-        return 0.5*np.exp(y/2.)*dn_dg(np.exp(y/2.))
+    if mode=='fft':
+        
+        # define convenience function for convolution
+        def fy(y):
+            return 0.5*np.exp(y/2.)*dn_dg(np.exp(y/2.))
+        
+        # define arrays over which the convenience functions are
+        # evaluated
+        y = np.arange(2.*np.log(gmin),2.*np.log(gmax),res)
+        x = np.arange(-25,15,res) # FIXME: hardcoded limits
+        
+        # this encodes the particle distribution, which represents the
+        # signal in the convolution
+        fyv = fy(y)
+        
+        # this encodes the single-particle spectrum, which represents the
+        # window in the convolution
+        gxyv = gxy(x)
+        
+        # do the convolution (scipy automatically switches to FFT if convenient)
+        hx = convolve(fyv,gxyv,mode='full')*res
+        
+        # reconstruct the frequency range
+        numin = gmin**2*1.5*np.sin(pitch)*q*B/(2*np.pi*m*c)*np.exp(x[0]) 
+        numax = gmax**2*1.5*np.sin(pitch)*q*B/(2*np.pi*m*c)*np.exp(x[-1])
+        nu0 = np.geomspace(numin,numax,len(hx))
+        
+        # compute normalization
+        K = normalization_Pnu(B,pitch,m,q)/(4.*np.pi)
     
-    # define arrays over which the convenience functions are
-    # evaluated
-    y = np.arange(2.*np.log(gmin),2.*np.log(gmax),res)
-    x = np.arange(-25,15,res) # FIXME: hardcoded limits
-    
-    # this encodes the particle distribution, which represents the
-    # signal in the convolution
-    fyv = fy(y)
-    
-    # this encodes the single-particle spectrum, which represents the
-    # window in the convolution
-    gxyv = gxy(x)
-    
-    # do the convolution (scipy automatically switches to FFT if convenient)
-    hx = convolve(fyv,gxyv,mode='full')
-    
-    # reconstruct the frequency range
-    numin = gmin**2*1.5*np.sin(pitch)*q*B/(2*np.pi*m*c)*np.exp(x[0]) 
-    numax = gmax**2*1.5*np.sin(pitch)*q*B/(2*np.pi*m*c)*np.exp(x[-1])
-    nu0 = np.geomspace(numin,numax,len(hx))
-    
-    # compute normalization
-    K = normalization_Pnu(B,pitch,m,q)/(4.*np.pi)
-    
-    return np.interp(nu,nu0,K*hx)
+        return np.interp(nu,nu0,K*hx)
 
+    else:
+        g = np.geomspace(dn_dg.gmin(),dn_dg.gmax,int(res**-1))[:,None]
+        Pnu = P_nu(nu,g,B,pitch,m,q)
+        ng = dn_dg(g)
+        
+        jnu = np.trapz(ng*Pnu,g,axis=0)/(4*np.pi)
+        
+        return jnu
+        
+                
+    
  
 
     
--- particle_distributions.py
+++ particle_distributions.py
@@ -1,4 +1,6 @@
 import numpy as np
+from scipy.special import gamma,gammaincc
+
 
 class powerlaw:
     
@@ -27,6 +29,9 @@ class powerlaw:
     def __call__(s,g):
         
         return (g>=s.gm)*(g<s.gmax)*(s.p-1.)*s.n/s.gm*(g/s.gm)**(-s.p)
+    
+    def gmin(self):
+        return self.gm
 
 
 class cooled_powerlaw:
@@ -56,10 +61,44 @@ class cooled_powerlaw:
         s.gp = np.minimum(gm,gc)
         s.g0 = np.maximum(gm,gc)
         s.q = np.where(gm<=gc,p,2.)
-        s.inv_norm = (s.g0/s.gp)**(1.-s.q)*(1./(1.-s.q)+1./s.p)-1./(1.-s.q)
+        x0 = s.g0/s.gp
+        s.inv_norm = ((x0**(1-s.q)-1.)/(1-s.q)+x0**(1-s.q)/s.p*(1.-(s.gmax/s.g0)**(-s.p)))
+    
+    def gmin(self):
+        return self.gp
     
     def __call__(s,g):
-        dndg = s.n/s.gp*(g/s.gp)**(-s.q)
-        dndg[g>s.g0] = s.n/s.gp*(s.g0/s.gp)**(-s.q)*(g[g>s.g0]/s.g0)**(-s.p-1.)
+        dndg = s.n/s.gp*np.where(g<=s.g0,(g/s.gp)**(-s.q),(s.g0/s.gp)**(-s.q)*(g/s.g0)**(-s.p-1.))
         
         return (g>=s.gp)*(g<s.gmax)*dndg/s.inv_norm
+
+class relativistic_maxwellian:
+
+    def __init__(s,n,TH,gmax=np.inf):
+        """
+        Class for broken power law particle Lorentz factor distribution,
+        such as that resulting from synchrotron cooling.
+        
+        Init parameters:
+        - n: total density (cm-3)
+        - TH: temperature parameter
+                
+        Methods:
+        __call__(): return density within (g,g+dg)
+        """
+        
+        s.n = n
+        s.TH = TH
+        if gmax==np.inf:
+            s.gmax = 1e4*TH
+        else:
+            s.gmax = gmax
+
+    def gmin(self):
+        return np.maximum(1.,self.TH/1e4)
+    
+    def __call__(s,g):
+        dndg = 0.5*s.n*g**2*np.exp(-g/s.TH)/(gamma(2)*gammaincc(2,s.TH**-1)*s.TH**3)*(g<s.gmax)
+        
+        return dndg
+    
